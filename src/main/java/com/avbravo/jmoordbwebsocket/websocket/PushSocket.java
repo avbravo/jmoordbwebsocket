60683f6aaa74bc1d675917ba26fd74cf40af1e7f
avbravo <avbravo@avbravo>
2019-05-30 14:11:38 -0500

    version incial

---
+++ src/main/java/com/avbravo/jmoordbwebsocket/websocket/PushSocket.java
@@ -0,0 +1,32 @@
+package com.avbravo.jmoordbwebsocket.websocket;
+
+import java.io.Serializable;
+import java.util.Calendar;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.enterprise.context.ApplicationScoped;
+import javax.faces.push.Push;
+import javax.faces.push.PushContext;
+import javax.inject.Inject;
+import javax.inject.Named;
+
+@Named
+@ApplicationScoped
+public class PushSocket implements Serializable {
+
+    private static final Logger LOG = Logger.getLogger(PushSocket.class.getName());
+
+    @Inject
+    @Push(channel = "clock")
+    private PushContext push;
+
+    public void clockAction() {
+        Calendar now = Calendar.getInstance();
+
+        String time = now.get(Calendar.HOUR_OF_DAY) + ":" + now.get(Calendar.MINUTE) + ":" + now.get(Calendar.SECOND);
+        LOG.log(Level.INFO, "Time: {0}", time);
+
+        push.send(time);
+    }
+
+}
